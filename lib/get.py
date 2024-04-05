a1dc4da64e8872c3410230b68af683b8b46c8ab1
estherwanjiku007 <estherwanjiqu@gmail.com>
2024-04-05 13:04:41 +0300

    Shows the httpget request of sqlalchemy

--- lib/get.py
+++ lib/get.py
@@ -3,8 +3,7 @@ import json
 
 url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/locations.json"
 
-response = requests.get(url)
+response=requests.get(url)
+json_content=json.loads(response.text)
 
-json_content = json.loads(response.text)
-
-print(json.dumps(json_content, indent=4))
+print(json.dumps(json_content,indent=4,sort_keys=True))
